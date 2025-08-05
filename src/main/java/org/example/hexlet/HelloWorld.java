1fad4305fcecdaf7bdf8597c1b09b63e92e6c56e
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-08-05 21:08:37 +0300

    add handler with param

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -10,6 +10,11 @@ public class HelloWorld {
         });
         // Описываем, что загрузится по адресу /
         app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+            ctx.result("Hello " + name);
+        });
+
         app.start(7070); // Стартуем веб-сервер
     }
 }
